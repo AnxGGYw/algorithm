236f19c99ee9580bcc3e9514bfb9573bbdfb768f
cggcbb <449567533@qq.com>
2019-02-15 16:55:20 +0800

    add quickSort 3 Ways

--- src/js/sort/QuickSort.js
+++ src/js/sort/QuickSort.js
@@ -2,7 +2,7 @@
  * @Description: 快速排序
  * @Author: cggcbb
  * @Date: 2019-02-13 15:14:49
- * @LastEditTime: 2019-02-15 16:10:39
+ * @LastEditTime: 2019-02-15 16:45:52
  */
 import util from '@/js/util/util'
 import InsertionSort from './InsertionSort'
@@ -13,6 +13,51 @@ const CRITICAL_POINT = 15
 export default class QuickSort {
   static sort(arr) {
     QuickSort._quickSort(arr, 0, arr.length - 1)
+  }
+
+  // 三路快排
+  static sort3Ways(arr) {
+    QuickSort._quickSort3Ways(arr, 0, arr.length - 1)
+  }
+  static _quickSort3Ways(arr, l, r) {
+    if (r - l < CRITICAL_POINT) {
+      InsertionSort.sortRange(arr, l, r)
+      return
+    }
+    let { lt, gt } = QuickSort._partition3Ways(arr, l, r)
+    QuickSort._quickSort3Ways(arr, l, lt - 1)
+    QuickSort._quickSort3Ways(arr, gt, r)
+  }
+  /**
+   * 三路快排partition
+   * @param {*} arr 
+   * @param {*} l 左边界
+   * @param {*} r 右边界
+   */
+  static _partition3Ways(arr, l, r) {
+    util.swap(arr, l, Math.floor((Math.random(r - l + 1) + l)))
+    let el = arr[l], // 当前 比较值
+        lt = l,      // arr[l + 1....lt] < el
+        i = l + 1,   // arr[lt + 1....i) == el
+        gt = r + 1   // arr[gt....r] > el
+    while(i < gt) {
+      if (arr[i] < el) {
+        util.swap(arr, i, lt + 1)
+        i ++
+        lt ++
+      } else if (arr[i] > el) {
+        util.swap(arr, i, gt - 1)
+        gt --
+      } else {
+        i ++
+      }
+    }
+    util.swap(arr, l, lt)
+    // 返回 lt 和 gt 的索引
+    return {
+      lt,
+      gt
+    }
   }
   
   // 对arr[l....r]部分进行快速排序
@@ -68,4 +113,5 @@ export default class QuickSort {
     util.swap(arr, l, j)
     return j
   }
+  
 }
--- src/js/test.js
+++ src/js/test.js
@@ -5,12 +5,14 @@ import SortTestHelper from '@/js/sort/SortTestHelper'
 import MergeSort from '@/js/sort/MergeSort'
 import QuickSort from '@/js/sort/QuickSort'
 
-// let randomArr = util.generateRandomArray(100000, 0, 10)
-let randomArr = util.generateNearlyOrderedArray(100, 100)
+let randomArr = util.generateRandomArray(100000, 0, 10)
+// let randomArr = util.generateNearlyOrderedArray(100, 100)
 let copyArr = util.copyArray(randomArr)
 let copyArr1 = util.copyArray(randomArr)
 let copyArr2 = util.copyArray(randomArr)
+let copyArr3 = util.copyArray(randomArr)
 SortTestHelper.testSort('SelectionSort', SelectionSort.sort, randomArr)
 SortTestHelper.testSort('InsertionSort', InsertionSort.sortNoneSwap, copyArr)
 SortTestHelper.testSort('MergeSort', MergeSort.sort, copyArr1)
-SortTestHelper.testSort('MergeSort', QuickSort.sort, copyArr2)
+SortTestHelper.testSort('QuickSort', QuickSort.sort, copyArr2)
+SortTestHelper.testSort('QuickSort 3 Ways', QuickSort.sort3Ways, copyArr3)
